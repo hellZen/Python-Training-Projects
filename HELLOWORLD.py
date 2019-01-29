5311854fd476c55e0e9bb30bed6384c262ceb9fc
hellZen <ausjohnston@gmail.com>
2019-01-29 16:55:55 -0500

    Create HELLOWORLD.py
    First 2.7 version of HELLOWORLD.  THe odd problew was that the html header had to be created correctly preceding the html:
    It looks like this
    Content-type: text/html \n\n
    
    Note the t two linebreaks..  THe html starts after this.
    
    Extraneous test code is still floating around at this point... :-(

---
+++ HELLOWORLD.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-# enable debugging
+import cgitb
+cgitb.enable()
+print ("Content-type: text/html\n\n")
+#print("<Content-Type: text/html;charset=utf-8>")
+# print ("<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">")
+print ("<html><head>")
+print ("<title>Beautiful</title>")
+print ("</head>")
+print("<body>")
+print (" ")
+print ("Hello World!")
+print ("<hr>")
+#print ("<address>Apache/2.4.18 (Ubuntu) Server at 10.0.0.81 Port 80</address>")
+print ("</body></html>")
